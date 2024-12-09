006d93fea9a973e9ae9fb69bdf0ec4b74285e90c
rezuan98 <rezuanahmmeds@gmail.com>
2024-12-09 13:02:26 +0600

    this is a initial commit with navbar and footer

---
+++ resources/views/frontend/includes/footer.blade.php
@@ -0,0 +1,82 @@
+<div class="footer_super_class">
+    <div class="footer_common_container">
+  
+      <footer class="footer text-white pt-4">
+   
+        <div class="footer_container">
+          <div class="row">
+            <!-- Customer Service -->
+            <div class="col-md-3">
+              <h5 class="">Customer Service</h5>
+              <ul class="list-unstyled mt-5 mb-5">
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Order Tracking</a></li>
+                <li><a href="#">Returns & Refunds</a></li>
+                <li><a href="#">Shipping Info</a></li>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Order Tracking</a></li>
+                <li><a href="#">Returns & Refunds</a></li>
+                <li><a href="#">Shipping Info</a></li>
+              </ul>
+            </div>
+      
+            <!-- About Us -->
+            <div class="col-md-3">
+              <h5>About Us</h5>
+              <ul class="list-unstyled mt-5 mb-5">
+                <li><a href="#">Our Story</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Sustainability</a></li>
+                <li><a href="#">Press Releases</a></li>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Order Tracking</a></li>
+                <li><a href="#">Returns & Refunds</a></li>
+                <li><a href="#">Shipping Info</a></li>
+              </ul>
+            </div>
+      
+            <!-- Popular Categories -->
+            <div class="col-md-3">
+              <h5>Popular Categories</h5>
+              <ul class="list-unstyled mt-5 mb-5">
+                <li><a href="#">Electronics</a></li>
+                <li><a href="#">Fashion</a></li>
+                <li><a href="#">Home & Kitchen</a></li>
+                <li><a href="#">Beauty & Health</a></li>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Order Tracking</a></li>
+                <li><a href="#">Returns & Refunds</a></li>
+                <li><a href="#">Shipping Info</a></li>
+              </ul>
+            </div>
+      
+            <!-- Contact & Social Media -->
+            <div class="col-md-3  ">
+              <h5>Contact Us</h5>
+              <p class="text-white mt-5"><i class="fa-solid fa-location-dot"> </i>  2D,Block-J,Baridhara,Dhaka</p>
+              <p class="text-white"><i class="fas fa-phone  text-white"></i> +1 (123) 456-7890</p>
+              <p class="text-white"><i class="fas fa-envelope text-white " ></i> support@yourstore.com</p>
+              <p class="text-white mt-5"><i class="fa-solid fa-location-dot"> </i>  2D,Block-J,Baridhara,Dhaka</p>
+              <p class="text-white"><i class="fas fa-phone  text-white"></i> +1 (123) 456-7890</p>
+              <p class="text-white"><i class="fas fa-envelope text-white " ></i> support@yourstore.com</p>
+              
+              <!-- Social Media Icons -->
+              <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+      
+          <!-- Footer Bottom -->
+          <div class="footer-bottom text-center pt-3">
+            <p>&copy; 2024 YourBrand. All rights reserved.</p>
+          </div>
+        </div>
+     
+      
+      </footer>
+    </div>
+  </div>
---
+++ resources/views/frontend/includes/navbar.blade.php
@@ -0,0 +1,34 @@
+<div class="nav_super_class">
+  <div class="nav_common_container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+              <!-- Brand Logo -->
+              <a class="navbar-brand" href="#">
+                  <img src="https://via.placeholder.com/150x40?text=BrandLogo" alt="Brand Logo">
+              </a>
+
+              <!-- Toggle Button for Mobile View -->
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span id="toggler" class="navbar-toggler-icon"></span>
+              </button>
+
+              <!-- Navbar Links -->
+              <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav mx-auto">
+                      <li class="nav-item">
+                          <a class="nav-link" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Services</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">About Us</a>
+                      </li>
+                  </ul>
+                  <!-- Free Consultation Button -->
+                  <a href="#" id="consultation" class="btn btn-consultation ms-lg-3">Get a Free Consultation</a>
+              </div>
+          </div>
+      </nav>
+  </div>
+</div>
